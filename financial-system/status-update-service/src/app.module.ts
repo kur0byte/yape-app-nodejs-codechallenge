bf7cd3e98d8de93333fb9a62f434e6e32740cbcb
gelatinaCosmica <santiagozapatabohorquez@hotmail.com>
2024-10-10 15:59:57 -0500

    chore: remove db init from app module

--- financial-system/status-update-service/src/app.module.ts
+++ financial-system/status-update-service/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { StatusUpdateModule } from './StatusUpdateService/StatusUpdateService.module';
+import { Transaction } from './StatusUpdateService/Transaction.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     StatusUpdateModule,
   ],
+  exports: [],
 })
 export class AppModule {}
