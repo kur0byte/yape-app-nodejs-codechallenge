e3aca0ed7b96df6483eb691e0b0a2715e28a971c
gelatinaCosmica <santiagozapatabohorquez@hotmail.com>
2024-10-10 12:35:55 -0500

    build: set to initialize only kafka ms

--- financial-system/anti-fraud-service/src/main.ts
+++ financial-system/anti-fraud-service/src/main.ts
@@ -1,26 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { AntiFraudModule } from './AntifraudSystem/AntifraudSystem.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AntiFraudModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: [process.env.KAFKA_BROKER],
+        },
+        consumer: {
+          groupId: 'antifraud',
+        },
+      },
+    }
+  );
+
   const configService = app.get(ConfigService);
+  console.log(`Connecting to Kafka broker: ${configService.get('KAFKA_BROKER')}`);
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.KAFKA,
-    options: {
-      client: {
-        brokers: [configService.get<string>('KAFKA_BROKER')],
-      },
-      consumer: {
-        groupId: 'anti-fraud-consumer',
-      },
-    },
-  });
-
-  await app.startAllMicroservices();
-  await app.listen(3002);
+  await app.listen();
+  console.log('Anti-Fraud microservice is listening');
 }
-
-bootstrap()
+bootstrap();
