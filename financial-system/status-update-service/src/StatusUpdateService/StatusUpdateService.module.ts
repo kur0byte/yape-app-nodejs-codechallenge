b07c1bd0b875604768a4a8b6e7e5a4a15520795e
gelatinaCosmica <santiagozapatabohorquez@hotmail.com>
2024-10-10 16:02:05 -0500

    chore: add typeorm module to status update service

--- financial-system/status-update-service/src/StatusUpdateService/StatusUpdateService.module.ts
+++ financial-system/status-update-service/src/StatusUpdateService/StatusUpdateService.module.ts
@@ -1,30 +1,27 @@
-import { Module } from '@nestjs/common';
 import { ClientsModule, KafkaOptions, Transport } from '@nestjs/microservices';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { StatusUpdateController } from './StatusUpdate.controller';
 import { StatusUpdateService } from './StatusUpdateService.service';
+import { Transaction } from './Transaction.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env'],
+      envFilePath: '.env',
     }),
-    
     ClientsModule.registerAsync([
       {
         name: 'KAFKA_SERVICE',
         imports: [ConfigModule],
-        useFactory: (configService: ConfigService): KafkaOptions => ({
+        useFactory: (configService: ConfigService) => ({
           transport: Transport.KAFKA,
           options: {
             client: {
               clientId: 'status-update-service',
               brokers: [configService.get<string>('KAFKA_BROKER')],
-              retry: {
-                initialRetryTime: 300,
-                retries: 12
-              },
             },
             consumer: {
               groupId: 'status-update',
@@ -36,11 +33,26 @@ import { StatusUpdateService } from './StatusUpdateService.service';
                 retries: 12
               }
             }
-          },
+          }
         }),
         inject: [ConfigService],
       },
-    ])
+    ]),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Transaction],
+        // synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([Transaction]),
   ],
   controllers: [StatusUpdateController],
   providers: [StatusUpdateService],
