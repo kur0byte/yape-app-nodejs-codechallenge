2177a69dc771e4ecfd84bd4184d74b06212e8c84
gelatinaCosmica <santiagozapatabohorquez@hotmail.com>
2024-10-10 12:46:32 -0500

    fix: project microservice initializer

--- financial-system/status-update-service/src/main.ts
+++ financial-system/status-update-service/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { StatusUpdateModule } from './StatusUpdateService/StatusUpdateService.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3003);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    StatusUpdateModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: [process.env.KAFKA_BROKER],
+        },
+        consumer: {
+          groupId: 'status-update',
+        },
+      },
+    }
+  );
+
+  const configService = app.get(ConfigService);
+  console.log(`Connecting to Kafka broker: ${configService.get('KAFKA_BROKER')}`);
+
+  await app.listen();
+  console.log('Status Update microservice is listening');
 }
 bootstrap();
