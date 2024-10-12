ad06dc80d6f4bb8f92ed035543f31b5eba2caf31
gelatinaCosmica <santiagozapatabohorquez@hotmail.com>
2024-10-12 16:39:18 -0500

    chore: removed proxy module and controller

--- financial-system/api-gateway/src/app.module.ts
+++ financial-system/api-gateway/src/app.module.ts
@@ -1,21 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
-import { ProxyModule } from './proxy/proxy.module';
-import { AllExceptionsFilter } from './filter/all-exceptions-filter';
 import { ProxyService } from './proxy/proxy.service';
+import { AllExceptionsFilter } from './filter/all-exceptions-filter';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ProxyModule,
   ],
   providers: [
     {
       provide: APP_FILTER,
       useClass: AllExceptionsFilter,
     },
-    // ProxyService
+    ProxyService
   ],
 })
 export class AppModule {}
